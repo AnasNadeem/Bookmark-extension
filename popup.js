ad2db2af645fdb732f34436e853bdea991c84322
Anas Nadeem <anas5678go@gmail.com>
2023-04-17 22:06:26 +0530

    v1 base files added

---
+++ popup.js
@@ -0,0 +1,26 @@
+import { getActiveTab, generateTags } from "./utils.js";
+
+const formUrl = document.getElementById("url");
+const formTitle = document.getElementById("title");
+const formTags = document.getElementById("tags");
+const tagsSuggestion = document.getElementById("tagsSuggestion");
+
+const currentTab = await getActiveTab();
+formUrl.value = currentTab.url;
+formTitle.value = currentTab.title;
+
+// Suggested tags based on site title
+const suggestiveTags = generateTags(currentTab.title);
+for (const tag of suggestiveTags) {
+    const tagElement = document.createElement("p");
+    tagElement.classList.add("tag");
+    tagElement.innerText = tag.toLowerCase();
+    tagsSuggestion.appendChild(tagElement);
+}
+
+tagsSuggestion.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tag")) {
+        formTags.value += e.target.innerText + " ,";
+        e.target.remove();
+    }
+});
---
+++ utils.js
@@ -0,0 +1,19 @@
+export async function getActiveTab() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+    return tabs[0];
+}
+
+export function generateTags(title) {
+    const tags = title.split(" ");
+    const commonWords = ['the', 'a', 'an', 'and', 'or', 'but', 'for', 'nor', 'so', 'yet', 'at', 'by', 'in', 'of', 'on', 'to', 'up', 'as', 'if', 'into', 'are', 'like', 'than', 'via', 'with', 'without', 'after', 'along', 'from', 'is', 'near', 'off', 'onto', 'out', 'over', 'past', 'under', 'within', 'about', 'above', 'across', 'against', 'amid', 'among', 'around', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'concerning', 'considering', 'despite', 'down', 'during', 'except', 'excepting', 'excluding', 'following', 'inside', 'minus', 'next', 'opposite', 'outside', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'through', 'toward', 'towards', 'underneath', 'unlike', 'until', 'upon', 'according', 'ahead', 'apart', 'aside', 'away', 'back']
+    tags.forEach((tag, index) => {
+        tags[index] = tag.replace(/[^a-zA-Z0-9]/g, "");
+    });
+    const filteredTags = tags.filter(tag => {
+        return tag.length >= 3 && tag.length <= 15 && !commonWords.includes(tag.toLowerCase());
+    });
+    return [... new Set(filteredTags)];
+}
