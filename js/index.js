3355163add91277ebb2a0a3cd8b56111c6e7c593
Anas Nadeem <anas5678go@gmail.com>
2023-05-20 07:12:40 +0530

    Bookmark CRUD integrated

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const BASE_API = "http://127.0.0.1:8000/api";
 const SITE_API = `${BASE_API}/site`;
 const TAG_API = `${BASE_API}/tag`;
+const BOOKMARK_API = `${BASE_API}/bookmark`;
 
 const siteLinkSidebar = document.getElementById("siteLinkSidebar");
 const tagLinkSidebar = document.getElementById("tagLinkSidebar");
+const bookmarkTbody = document.getElementById("bookmarkTbody");
 
 const readLocalStorage = async (key) => {
   return new Promise((resolve, reject) => {
@@ -21,12 +23,12 @@ const readLocalStorage = async (key) => {
 const getSite = async () => {
   const user = await readLocalStorage("user");
   const token = user.token;
-  console.log(token);
   const headersData = {
     Authorization: `${token}`,
     "Content-Type": "application/json",
   };
   const headersMethodGet = { method: "GET", headers: headersData };
+
   fetch(SITE_API, headersMethodGet)
     .then((resp) => {
       return resp.json();
@@ -54,6 +56,7 @@ const getTags = async () => {
     "Content-Type": "application/json",
   };
   const headersMethodGet = { method: "GET", headers: headersData };
+
   fetch(TAG_API, headersMethodGet)
     .then((resp) => {
       return resp.json();
@@ -73,7 +76,49 @@ const getTags = async () => {
     });
 };
 
+// GET BOOKMARK
+const getBookmarks = async () => {
+  const user = await readLocalStorage("user");
+  const token = user.token;
+  const headersData = {
+    Authorization: `${token}`,
+    "Content-Type": "application/json",
+  };
+  const headersMethodGet = { method: "GET", headers: headersData };
+
+  fetch(BOOKMARK_API, headersMethodGet)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      data.forEach((bookmark) => {
+        bookmarkTbody.innerHTML += `
+        <tr id="bookmark${bookmark.id}">
+          <td>${bookmark.title}</td>
+          <td>
+            <a href="${bookmark.url}" target="_blank">
+              ${bookmark.url}
+            </a>
+          </td>
+          <td>
+            <ul class="tableTags">
+              ${bookmark.tags.map((tag) => {
+                return `<li class="tableTag">#${tag.name}</li>`;
+              })}
+            </ul>
+          </td>
+          <td>
+            <i class="fas fa-edit"></i>
+            <i class="fa-solid fa-trash trashIcon"></i>
+          </td>
+        </tr>
+        `;
+      });
+    });
+};
+
 window.onload = () => {
   getSite();
   getTags();
+  getBookmarks();
 };
